bb6a40e90fb7dcbb4cc73698bf5f445c61f40afd
sheucm <st0502123@hotmail.com.tw>
2014-04-15 10:14:06 +0800

    grab record func

--- public/javascripts/storageMyProgram.js
+++ public/javascripts/storageMyProgram.js
@@ -1,5 +1,7 @@
 $(document).on("pagecreate", '#myProgramPage', function() {
    $('#deleteAll_myProgram').on("click", clearAllProgram);
+
+   loadList();
 });
 
 //Example of localStorage
@@ -35,6 +37,50 @@ function removeProgram(id){
     $("#myProgram_"+id).remove().listview().listview('refresh');
 }
 
+function add2List(id){
+    var list = localStorage.getItem("MYPROGRAM_LIST_RECORDED");
+
+    if(list == null)    list = id;
+    else                list = list + "," + id;
+    localStorage.setItem("MYPROGRAM_LIST_RECORDED", list);
+}
+
+function loadList(){
+    var listData = localStorage.getItem("MYPROGRAM_LIST_RECORDED");
+    if(listData != null){
+        listData = listData.split(",");
+        for(i=0; i<listData.length; i++){
+            var datas = listData[i].split("***");
+            var id = datas[0];
+            var type = datas[1];
+
+            //alert("id:"+id+" type:"+type);
+
+            if(type == 1){
+                append2MyProgram1(id);
+            }
+            else if(type == 2){
+                append2MyProgram2(id);
+            }
+            else if(type == 3){
+                append2MyProgram3(id);
+            }
+            else if(type == 4){
+                append2MyProgram4(id);
+            }
+            else if(type == 5){
+                append2MyProgram5(id);
+            }
+            else if(type == 6){
+                append2MyProgram6(id);
+            }
+            else{
+               console.log("[Error]in storageMyProgram.js loadList(): the 'type' var should not out of 1~6");
+            }
+        }
+    }
+    
+}
 
 /*******************************************************************************************************************//*
 Classify with 6 storage types that I defined:
@@ -54,13 +100,25 @@ the types of each day
 *//*-*****************************************************************************************************************/
 /*-------------------------------TYPE 1---------------------------------------*/
 function storageType1(time, id, panel, chair, venue){ 
-    var data;
-    localStorage.setItem(id, panel + "***" + chair + "***" + venue);
-    data = localStorage.getItem(id).split("***");
-    append2MyProgram1(time, id, panel, chair, venue);    
+    localStorage.setItem(id, time + "***" + panel + "***" + chair + "***" + venue);
+    add2List(id+"***1");
+    append2MyProgram1(id);    
 }
 
-function append2MyProgram1(time, id, panel, chair, venue){
+function append2MyProgram1(id){
+    var data;
+    var time, panel, chair, venue;
+
+    data = localStorage.getItem(id);
+
+    if(data == null) return;
+
+    data = data.split("***");
+    time = data[0];
+    panel = data[1];
+    chair = data[2];
+    venue = data[3]; 
+
     if($('#myProgram_'+id).length <= 0){
         $('#myProgramList').append('<li id="myProgram_'+id+'"><a href="#'+id+'">'
                                    +'<span style="display:inline-block"><h2 style="color:#E03A3A"> Panel: </h2></span>'
@@ -79,13 +137,26 @@ function append2MyProgram1(time, id, panel, chair, venue){
 }
 /*-------------------------------TYPE 2---------------------------------------*/
 function storageType2(time, id, title, speaker, chair, venue){
-    var data;
-    localStorage.setItem(id, title + "***" + speaker + "***" + chair + "***" + venue);
-    data = localStorage.getItem(id).split("***");
-    append2MyProgram2(time, id, title, speaker, chair, venue);    
+    localStorage.setItem(id, time + "***" + title + "***" + speaker + "***" + chair + "***" + venue);
+    add2List(id+"***2");
+    append2MyProgram2(id);    
 }
 
-function append2MyProgram2(time, id, title, speaker, chair, venue){
+function append2MyProgram2(id){
+    var data;
+    var time, title, speaker, chair, venue;
+
+    data = localStorage.getItem(id);
+
+    if(data == null) return;
+
+    data = data.split("***");
+    time = data[0];
+    title = data[1];
+    speaker = data[2];
+    chair = data[3];
+    venue = data[4]; 
+
     if($('#myProgram_'+id).length <= 0){
         $('#myProgramList').append('<li id="myProgram_'+id+'"><a href="#'+id+'">'
                                  +'<h1 id="keynoteTitle">'+title+'</h1>'
@@ -106,13 +177,24 @@ function append2MyProgram2(time, id, title, speaker, chair, venue){
 }
 /*-------------------------------TYPE 3---------------------------------------*/
 function storageType3(time, id, session, venue){
-    var data;
-    localStorage.setItem(id, session + "***" + venue);
-    data = localStorage.getItem(id).split("***"); 
-    append2MyProgram3(time, id, session, venue);    
+    localStorage.setItem(id, time + "***" + session + "***" + venue);
+    add2List(id+"***3");
+    append2MyProgram3(id);    
 }
 
-function append2MyProgram3(time, id, session, venue){
+function append2MyProgram3(id){
+    var data;
+    var time, session, venue;
+
+    data = localStorage.getItem(id);
+
+    if(data == null) return;
+
+    data = data.split("***");
+    session = data[0];
+    venue = data[1]; 
+
+
     if($('#myProgram_'+id).length <= 0){
         $('#myProgramList').append('<li id="myProgram_'+id+'"><a href="#'+id+'">'
                                    +'<span style="display:inline-block"><h1 style="color:#E03A3A"> Session: </h1></span>'
@@ -128,13 +210,26 @@ function append2MyProgram3(time, id, session, venue){
 
 /*-------------------------------TYPE 4---------------------------------------*/
 function storageType4(time, id, name, title, lecturer, venue){
-    var data;
-    localStorage.setItem(id, name + "***" + title + "***" + lecturer + "***" + venue);
-    data = localStorage.getItem(id).split("***");
-    append2MyProgram4(time, id, name, title, lecturer, venue);    
+    localStorage.setItem(id, time + "***" + name + "***" + title + "***" + lecturer + "***" + venue);
+    add2List(id+"***4");
+    append2MyProgram4(id);    
 }
 
-function append2MyProgram4(time, id, name, title, lecturer, venue){
+function append2MyProgram4(id){
+    var data;
+    var time, name, title, lecturer, venue;
+
+    data = localStorage.getItem(id);
+
+    if(data == null) return;
+
+    data = data.split("***");
+    time = data[0];
+    name = data[1];
+    title = data[2];
+    lecturer = data[3];
+    venue = data[4]; 
+
     if($('#myProgram_'+id).length <= 0){
       $('#myProgramList').append('<li id="myProgram_'+id+'"><a href="#'+id+'">'
                                      +'<span style="display:inline-block"><h2 style="color:#E03A3A">'+name+'</h2></span>'
@@ -153,13 +248,25 @@ function append2MyProgram4(time, id, name, title, lecturer, venue){
 }
 /*-------------------------------TYPE 5---------------------------------------*/
 function storageType5(time, id, session, chair, venue){
-    var data;
-    localStorage.setItem(id, session + "***" + chair + "***" + venue);
-    data = localStorage.getItem(id).split("***");
-    append2MyProgram5(time, id, session, chair, venue);    
+    localStorage.setItem(id, time + "***" + session + "***" + chair + "***" + venue);
+    add2List(id+"***5");
+    append2MyProgram5(id);    
 }
 
-function append2MyProgram5(time, id, session, chair, venue){
+function append2MyProgram5(id){
+    var data;
+    var time, session, chair, venue;
+
+    data = localStorage.getItem(id);
+
+    if(data == null) return;
+
+    data = data.split("***");
+    time = data[0];
+    session = data[1];
+    chair = data[2];
+    venue = data[3]; 
+
     if($('#myProgram_'+id).length <= 0){
       $('#myProgramList').append('<li id="myProgram_'+id+'"><a href="#'+id+'">'
                                    +'<span style="display:inline-block"><h1 style="color:#E03A3A"> Session: </h1></span>'
@@ -179,13 +286,24 @@ function append2MyProgram5(time, id, session, chair, venue){
 
 /*-------------------------------TYPE 6---------------------------------------*/
 function storageType6(time, id, workshopName, venue){
-    var data;
-    localStorage.setItem(id, workshopName + "***" + venue);
-    data = localStorage.getItem(id).split("***");
-    append2MyProgram6(time, id, workshopName, venue);    
+    localStorage.setItem(id, time + "***" + workshopName + "***" + venue);
+    add2List(id+"***6");
+    append2MyProgram6(id);    
 }
 
-function append2MyProgram6(time, id, workshopName, venue){
+function append2MyProgram6(id){
+    var data;
+    var time, workshopName, venue;
+
+    data = localStorage.getItem(id);
+
+    if(data == null) return;
+
+    data = data.split("***");
+    time = data[0];
+    workshopName = data[1];
+    venue = data[2]; 
+
     if($('#myProgram_'+id).length <= 0){
       $('#myProgramList').append('<li id="myProgram_'+id+'"><a href="#'+id+'">'
                          +'<span style="display:inline-block"><h1 style="color:#E03A3A"> WorkShop Name: </h1></span>'
