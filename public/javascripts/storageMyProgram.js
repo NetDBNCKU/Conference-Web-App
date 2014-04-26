8d3c42095679e541ac3329747bb376cd6fac9e4e
sheucm <st0502123@hotmail.com.tw>
2014-04-26 11:29:34 +0800

    test

--- public/javascripts/storageMyProgram.js
+++ public/javascripts/storageMyProgram.js
@@ -3,6 +3,7 @@ window.pageMap = {};
 
 $(document).ready(function(){
     $("#btn_myProgram").on("click", loadList);
+    alert("hello");
 });
 
 $(document).on("pagecreate", '#myProgramPage', function() {
