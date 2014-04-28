7992ce5e2b74ce6f315ed24e143f595677477387
ArvinH <arvin0731@gmail.com>
2014-04-28 19:35:34 +0800

    ScrollTo function

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,6 +1,26 @@
 $(document).on('pagecreate','#a_OptionA',function(){
-     $('#aa').on('click',function(){
-	         $('body').scrollTo('#xx');
-				   });
-
+     $('#TIA').off('click').on('click',function(){
+	     $('#xTIA').ScrollTo({
+	       duration: 1000,
+	       easing: 'linear'
+	     });
+     });
+     $('#TTaoyuan').off('click').on('click',function(){
+	     $('#xTTaoyuan').ScrollTo({
+	       duration: 1000,
+	       easing: 'linear'
+	     });
+     });
+     $('#TTainan').off('click').on('click',function(){
+	     $('#xTTainan').ScrollTo({
+	       duration: 1000,
+	       easing: 'linear'
+	     });
+     });
+     $('#TRS').off('click').on('click',function(){
+	     $('#xTRS').ScrollTo({
+	       duration: 1000,
+	       easing: 'linear'
+	     });
+     });
 });
